fe3b22f3671903168dbb067d2dc013a2de4a6259
Baptiste Busch <baptiste.busch@gmail.com>
2019-09-04 10:07:37 +0200

    Correct missing path in cmake

--- lib/build.sh
+++ lib/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CURRPATH=${PWD}
 #cd ~/lib/passive_ds_control && mkdir -p build && cd build && cmake cmake -Druntests=ON .. && make && sudo make install
-cd ${CURRPATH}/protocol_buffers && mkdir -p build && cd build && cmake && make && sudo make install
-cd ${CURRPATH}/state_representation && mkdir -p build && cd build && cmake && make && sudo make install
-cd ${CURRPATH}/dynamical_systems && mkdir -p build && cd build && cmake && make && sudo make install
+cd ${CURRPATH}/protocol_buffers && mkdir -p build && cd build && cmake .. && make && sudo make install
+cd ${CURRPATH}/state_representation && mkdir -p build && cd build && cmake .. && make && sudo make install
+cd ${CURRPATH}/dynamical_systems && mkdir -p build && cd build && cmake .. && make && sudo make install
