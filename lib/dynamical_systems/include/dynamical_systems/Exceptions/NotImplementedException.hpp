35b494699c5c14f16ae69ad6fe59b9af8d4f670c
Baptiste Busch <baptiste.busch@gmail.com>
2019-09-06 09:43:50 +0200

    Add NotImplementedException

---
+++ lib/dynamical_systems/include/dynamical_systems/Exceptions/NotImplementedException.hpp
@@ -0,0 +1,21 @@
+
+
+#ifndef DYNAMICALSYSTEMS_EXCEPTIONS_NOTIMPLEMENTEDEXCEPTION_H_
+#define DYNAMICALSYSTEMS_EXCEPTIONS_NOTIMPLEMENTEDEXCEPTION_H_
+
+#include <iostream>
+#include <exception>
+
+namespace DynamicalSystems
+{
+	namespace Exceptions
+	{
+		class NotImplementedException : public std::logic_error
+		{
+		public:
+			NotImplementedException(const std::string& msg) : logic_error(msg)
+			{};
+		};
+	}
+}
+#endif
