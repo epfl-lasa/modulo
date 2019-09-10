6964c23f218926b4ea1ea3f436055b5c67777995
Baptiste Busch <baptiste.busch@gmail.com>
2019-09-10 11:27:49 +0200

    Add a class to represent the jacbobian matrix of a robot

---
+++ lib/state_representation/include/state_representation/Robot/JacobianMatrix.hpp
@@ -0,0 +1,271 @@
+/**
+ * @class JacobianMatrix
+ * @brief Class to define a robot Jacobian matrix
+ * @author Baptiste Busch
+ * @date 2019/09/09
+ */
+
+#ifndef STATEREPRESENTATION_ROBOT_JACOBIANMATRIX_H_
+#define STATEREPRESENTATION_ROBOT_JACOBIANMATRIX_H_
+
+#include <eigen3/Eigen/Core>
+#include "state_representation/State.hpp"
+#include "state_representation/Cartesian/CartesianVelocity.hpp"
+#include "state_representation/Joint/JointVelocities.hpp"
+#include "state_representation/Exceptions/IncompatibleSizeException.hpp"
+
+using namespace StateRepresentation::Exceptions;
+
+namespace StateRepresentation 
+{
+	class CartesianVelocity;
+
+	class JointVelocities;
+
+	class JacobianMatrix: public State
+	{
+	private:
+		std::vector<std::string> joint_names; ///< names of the joints
+		unsigned int nb_rows; ///< number of rows
+		unsigned int nb_cols; ///< number of columns
+		Eigen::MatrixXd data; ///< internal storage of the jacobian matrix
+
+	public:
+		/**
+	 	 * @brief Constructor with name and number of joints provided
+	 	 * @brief name the name of the robot associated to
+	 	 * @brief nb_joints the number of joints
+	     */
+		explicit JacobianMatrix(const std::string& robot_name, unsigned int nb_joints);
+
+		/**
+	 	 * @brief Constructor with name and list of joint names provided
+	 	 * @brief name the name of the robot associated to
+	 	 * @brief joint_names the list of joint names
+	     */
+		explicit JacobianMatrix(const std::string& robot_name, const std::vector<std::string>& joint_names);
+
+		/**
+	 	 * @brief Constructor with name and jacobian matrix as eigen matrix
+	 	 * @brief name the name of the robot associated to
+	 	 * @brief data the value of the jacobian matrix
+	     */
+		explicit JacobianMatrix(const std::string& robot_name, const Eigen::MatrixXd& data);
+
+		/**
+	 	 * @brief Constructor with name, list of joint names and jacobian matrix as eigen matrix
+	 	 * @brief name the name of the robot associated to
+	 	 * @brief joint_names the list of joint names
+	 	 * @brief data the value of the jacobian matrix
+	     */
+		explicit JacobianMatrix(const std::string& robot_name, const std::vector<std::string>& joint_names, const Eigen::MatrixXd& data);
+
+		/**
+	 	 * @brief Copy constructor of a JacobianMatrix
+	     */
+		JacobianMatrix(const JacobianMatrix& jacobian);
+
+		/**
+	 	 * @brief Getter of the nb_rows from the attributes
+	     */
+		unsigned int get_nb_rows() const;
+
+		/**
+		 * @brief Setter of the nb_rows
+		 */
+		void set_nb_rows(unsigned int nb_rows);
+
+		/**
+		 * @brief Setter of the nb_cols
+		 */
+		void set_nb_cols(unsigned int nb_cols);
+
+		/**
+	 	 * @brief Getter of the nb_cols from the attributes
+	     */
+		unsigned int get_nb_cols() const;
+
+		/**
+	 	 * @brief Getter of the names attribute
+	     */
+		const std::vector<std::string>& get_joint_names() const;
+
+		/**
+	 	 * @brief Setter of the names attribute from the number of joints
+	     */
+		void set_joint_names(unsigned int nb_joints);
+
+		/**
+	 	 * @brief Setter of the names attribute
+	     */
+		void set_joint_names(const std::vector<std::string>& names);
+
+		/**
+	 	 * @brief Getter of the data attribute
+	     */
+		const Eigen::MatrixXd& get_data() const;
+
+		/**
+	 	 * @brief Setter of the data attribute
+	     */
+		void set_data(const Eigen::MatrixXd& data);
+
+		/**
+	 	 * @brief Check if the jacobian matrix is compatible for operations with the state given as argument
+	 	 * @param state the state to check compatibility with
+	     */
+		bool is_compatible(const State& state) const;
+
+		/**
+	 	 * @brief Initialize the matrix to a zero value
+	     */
+		void initialize();
+
+		/**
+		 * @brief Return the transpose of the jacobian matrix
+		 * @return the JacobianMatrix transposed
+		 */
+		const JacobianMatrix transpose();
+
+		/**
+	 	 * @brief Overload the * operator with an non specific matrix
+	 	 * @param matrix the vector to multiply with
+	 	 * @return the vector multiply by the jacobian matrix
+	     */
+		const Eigen::MatrixXd operator*(const Eigen::MatrixXd& matrix) const;
+
+		/**
+	 	 * @brief Overload the * operator with a JointVelocities
+	 	 * @param dq the joint velocity to multiply with
+	 	 * @return this result into the CartesianVelocity of the end effector
+	 	 * the name of the output CartesianVelocity will be "robot"_end_effector and
+	 	 * the reference frame will be "robot"_base 
+	     */
+		const CartesianVelocity operator*(const JointVelocities& dq) const;
+
+		/**
+	 	 * @brief Solve the system dX = J*dq to obtain dq which is more efficient than multiplying with the pseudo-inverse
+	 	 * @param dX the cartesian velocity to multiply with
+	 	 * @return this result into a JointVelocities
+	     */
+		const JointVelocities solve(const CartesianVelocity& dX) const;
+
+		/**
+	 	 * @brief Overload the * operator with a CartesianVelocity. This is equivalent to using the solve function
+	 	 * @param dX the cartesian velocity to multiply with
+	 	 * @return this result into a JointVelocities
+	     */
+		const JointVelocities operator*(const CartesianVelocity& dX) const;
+
+		/**
+		 * @brief Overload the () operator in a non const fashion to modify the value at given (row, col)
+		 * @param row the index of the row
+		 * @param the index of the column
+		 * @return the reference to the value at the given row and column
+		 */
+		double& operator()(unsigned int row, unsigned int col);
+
+		/**
+		 * @brief Overload the () operator const fashion to access the value at given (row, col)
+		 * @param row the index of the row
+		 * @param the index of the column
+		 * @return the const reference to the value at the given row and column
+		 */
+		const double& operator()(unsigned int row, unsigned int col) const;
+
+		/**
+	 	 * @brief Overload the ostream operator for printing
+	 	 * @param os the ostream to append the string representing the matrix to
+	 	 * @param matrix the matrix to print
+	 	 * @return the appended ostream
+	     */
+		friend std::ostream& operator<<(std::ostream& os, const JacobianMatrix& matrix);
+	};
+
+	inline unsigned int JacobianMatrix::get_nb_rows() const
+	{
+		return this->nb_rows;
+	}
+
+	inline unsigned int JacobianMatrix::get_nb_cols() const
+	{
+		return this->nb_cols;
+	}
+
+	inline void JacobianMatrix::set_nb_rows(unsigned int nb_rows)
+	{
+		this->nb_rows = nb_rows;
+	}
+
+	inline void JacobianMatrix::set_nb_cols(unsigned int nb_cols)
+	{
+		this->nb_cols = nb_cols;
+	}
+
+	inline const std::vector<std::string>& JacobianMatrix::get_joint_names() const
+	{
+		return this->joint_names;
+	}
+
+	inline void JacobianMatrix::set_joint_names(unsigned int nb_joints)
+	{
+		this->joint_names.resize(nb_joints);
+		for(unsigned int i=0; i<nb_joints; ++i)
+		{
+			this->joint_names[i] = "joint" + std::to_string(i);
+		}
+		this->initialize();
+	}
+
+	inline void JacobianMatrix::set_joint_names(const std::vector<std::string>& joint_names)
+	{
+		this->joint_names = joint_names;
+		this->initialize();
+	}
+
+	inline const Eigen::MatrixXd& JacobianMatrix::get_data() const
+	{
+		return this->data;
+	}
+
+	inline void JacobianMatrix::set_data(const Eigen::MatrixXd& data)
+	{
+		if(this->get_nb_rows() != data.rows() || this->get_nb_cols() != data.cols()) throw IncompatibleSizeException("Input matrix is of incorrect size");
+		this->set_filled();
+		this->data = data;
+	}
+
+	inline bool JacobianMatrix::is_compatible(const State& state) const
+	{
+		bool compatible = false;
+		if(state.get_type() == "JointState")
+		{
+			compatible = (this->get_name() == state.get_name()) && (this->get_nb_cols() == static_cast<const JointState&>(state).get_size());
+			if(compatible)
+			{
+				for(unsigned int i=0; i<this->get_nb_cols(); ++i) compatible = (compatible && this->joint_names[i] == static_cast<const JointState&>(state).get_names()[i]);
+			}
+		}
+		// there is no possibilities to check that a correct frame associated to the robot is sent
+		else if(state.get_type() == "CartesianState")
+		{
+			compatible = true;
+		}
+		return compatible;
+	}
+
+	inline double& JacobianMatrix::operator()(unsigned int row, unsigned int col)
+	{
+		if(row > this->get_nb_rows()) throw std::out_of_range("Given row is out of range: number of rows = " + this->get_nb_rows());
+		if(col > this->get_nb_cols()) throw std::out_of_range("Given column is out of range: number of columns = " + this->get_nb_cols());
+		return this->data(row, col);
+	}
+
+	inline const double& JacobianMatrix::operator()(unsigned int row, unsigned int col) const
+	{
+		if(row > this->get_nb_rows()) throw std::out_of_range("Given row is out of range: number of rows = " + this->get_nb_rows());
+		if(col > this->get_nb_cols()) throw std::out_of_range("Given column is out of range: number of columns = " + this->get_nb_cols());
+		return this->data(row, col);
+	}
+}
+#endif
---
+++ lib/state_representation/src/Robot/JacobianMatrix.cpp
@@ -0,0 +1,116 @@
+#include "state_representation/Robot/JacobianMatrix.hpp"
+#include "state_representation/Exceptions/IncompatibleStatesException.hpp"
+#include "state_representation/Exceptions/EmptyStateException.hpp"
+
+namespace StateRepresentation 
+{
+	JacobianMatrix::JacobianMatrix(const std::string& robot_name, unsigned int nb_joints):
+	State("JacobianMatrix", robot_name), nb_rows(6), nb_cols(nb_joints)
+	{
+		this->set_joint_names(nb_joints);
+	}
+
+	JacobianMatrix::JacobianMatrix(const std::string& robot_name, const std::vector<std::string>& joint_names):
+	State("JacobianMatrix", robot_name), nb_rows(6), nb_cols(joint_names.size())
+	{
+		this->set_joint_names(joint_names);
+	}
+
+	JacobianMatrix::JacobianMatrix(const std::string& robot_name, const Eigen::MatrixXd& data):
+	State("JacobianMatrix", robot_name), nb_rows(data.rows()), nb_cols(data.cols())
+	{
+		this->set_joint_names(this->get_nb_cols());
+		this->set_data(data);
+	}
+
+	JacobianMatrix::JacobianMatrix(const std::string& robot_name, const std::vector<std::string>& joint_names, const Eigen::MatrixXd& data):
+	State("JacobianMatrix", robot_name), nb_rows(data.rows()), nb_cols(joint_names.size())
+	{
+		this->set_joint_names(joint_names);
+		this->set_data(data);
+	}
+
+	JacobianMatrix::JacobianMatrix(const JacobianMatrix& jacobian):
+	State(jacobian), joint_names(jacobian.joint_names), data(jacobian.data)
+	{}
+
+	void JacobianMatrix::initialize()
+	{
+		this->State::initialize();
+		this->data.resize(this->nb_rows, this->nb_cols);
+		this->data.setZero();
+	}
+
+	const JacobianMatrix JacobianMatrix::transpose()
+	{
+		JacobianMatrix result(*this);
+		result.set_nb_cols(this->get_nb_rows());
+		result.set_nb_rows(this->get_nb_cols());
+		result.set_data(result.get_data().transpose());
+		return result;
+	}
+
+	const Eigen::MatrixXd JacobianMatrix::operator*(const Eigen::MatrixXd& matrix) const
+	{
+		if(this->is_empty()) throw EmptyStateException(this->get_name() + " state is empty");
+		if(matrix.cols() != this->get_nb_cols()) throw IncompatibleSizeException("Input matrix is of incorrect size");
+		Eigen::MatrixXd result = this->get_data() * matrix;
+		return result;
+	}
+
+	const CartesianVelocity JacobianMatrix::operator*(const JointVelocities& dq) const
+	{
+		if(this->is_empty()) throw EmptyStateException(this->get_name() + " state is empty");
+		if(dq.is_empty()) throw EmptyStateException(dq.get_name() + " state is empty");
+		if(!this->is_compatible(dq)) throw IncompatibleStatesException("The JacobianMatrix and the input JointVelocities are incompatible");
+		Eigen::Matrix<double, 6, 1> twist = this->get_data() * dq.get_velocities();
+		// crate a CartesianVelocity with name "robot"_end_effector and reference frame "robot"_base
+		CartesianVelocity result(this->get_name() + "_end_effector", twist, this->get_name() + "_base");
+		return result;
+	}
+
+	const JointVelocities JacobianMatrix::solve(const CartesianVelocity& dX) const
+	{
+		if(this->is_empty()) throw EmptyStateException(this->get_name() + " state is empty");
+		if(dX.is_empty()) throw EmptyStateException(dX.get_name() + " state is empty");
+		// extract the velocity as a 6d vector
+		auto twist = dX.get_twist();
+		// this use the solve operation instead of using the inverse or pseudo-inverse of the jacobian
+		Eigen::VectorXd joint_velocities = this->get_data().colPivHouseholderQr().solve(twist);
+		// return a JointVelocities state
+		JointVelocities result(this->get_name(), this->get_joint_names(), joint_velocities);
+		return result;
+	}
+
+	const JointVelocities JacobianMatrix::operator*(const CartesianVelocity& dX) const
+	{
+		return this->solve(dX);
+	}
+
+	std::ostream& operator<<(std::ostream& os, const JacobianMatrix& matrix)
+	{
+		if(matrix.is_empty())
+		{
+			os << "Empty JacobianMatrix";
+		}
+		else
+		{
+			os << matrix.get_name() << " JacobianMatrix" << std::endl;
+			os << "joint names: [";
+			for(auto& n:matrix.get_joint_names()) os << n << ", ";
+			os << "]" << std::endl;
+			os << "number of rows: " << matrix.get_nb_rows() << std::endl;
+			os << "number of columns: " << matrix.get_nb_cols() << std::endl;
+			for(unsigned int i = 0; i < matrix.get_nb_rows(); ++i)
+			{
+				os << "| " << matrix(i,0);
+				for(unsigned int j = 1; j < matrix.get_nb_cols(); ++j)
+				{
+					os << ", " << matrix(i,j);
+				}
+				os << " |" << std::endl;
+			}
+		}
+		return os;
+	}
+}
