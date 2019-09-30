70deb1a984e9efe51d0029c4b43a1201b74b112c
Baptiste Busch <baptiste.busch@gmail.com>
2019-09-30 09:01:59 +0200

    Put a cpp folder to include a python version of the lib

--- source/lib/build.sh
+++ source/lib/build.sh
@@ -2,5 +2,5 @@
 CURRPATH=${PWD}
 #cd ~/lib/passive_ds_control && mkdir -p build && cd build && cmake cmake -Druntests=ON .. && make && sudo make install
 cd "${CURRPATH}/protocol_buffers" && mkdir -p build && cd build && cmake .. && make && sudo make install
-cd "${CURRPATH}/state_representation" && mkdir -p build && cd build && cmake .. && make && sudo make install
+cd "${CURRPATH}/state_representation/cpp" && mkdir -p build && cd build && cmake .. && make && sudo make install
 cd "${CURRPATH}/dynamical_systems" && mkdir -p build && cd build && cmake .. && make && sudo make install
