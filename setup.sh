5da8c2ab58bf54c05c5e9ca0702a9fc92664becf
Baptiste Busch <baptiste@aica.tech>
2019-09-30 07:56:08 +0200

    Remove unnecessary script

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-CURRPATH=${PWD}
-git submodule update --init --recursive
-cd "${CURRPATH}/lib/dynamical_systems" && git checkout master && git pull 
-cd "${CURRPATH}/lib/protocol_buffers" && git checkout master && git pull
-cd "${CURRPATH}/lib/state_representation" && git checkout master && git pull
-cd "${CURRPATH}/src/modulo_core" && git checkout master && git pull
-cd "${CURRPATH}/src/modulo_msgs" && git checkout master && git pull
