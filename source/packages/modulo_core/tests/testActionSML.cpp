7aebba10e7f00542485c74770982412db4836c2f
Justine Costaz <justine.costaz@live.fr>
2020-04-21 11:11:34 +0200

    Test for state machine (Problem of warning with inconsistent states)

---
+++ source/packages/modulo_core/tests/testActionSML.cpp
@@ -0,0 +1,290 @@
+#include "modulo_core/Action.hpp"
+#include "modulo_core/Monitor.hpp"
+#include "modulo_core/Visualizer.hpp"
+#include "dynamical_systems/Linear.hpp"
+#include "rcutils/cmdline_parser.h"
+#include <eigen3/Eigen/Core>
+#include <iostream>
+#include <exception>
+#include <boost/sml.hpp>
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/int64.hpp"
+
+int e = 10;
+namespace sml = boost::sml;
+
+
+namespace {
+
+	struct e_start{}; //start
+	struct e0 {}; //home
+	struct e1 {}; //action1
+	struct e2 {}; //action2
+}
+
+class MoveAction : public Modulo::Actions::Action<StateRepresentation::CartesianState>
+{
+private:
+	std::shared_ptr<StateRepresentation::CartesianPose> target_pose;
+
+public:
+	template <typename DurationT>
+	explicit MoveAction(const std::string& node_name, const std::chrono::duration<int64_t, DurationT>& period) :
+	Action<StateRepresentation::CartesianState>(std::make_shared<StateRepresentation::CartesianPose>("robot_test"), std::make_shared<StateRepresentation::CartesianTwist>("robot_test"), node_name, period, false),
+	target_pose(std::make_shared<StateRepresentation::CartesianPose>("attractor", Eigen::Vector3d::Random()))
+	{}
+
+	bool on_configure()
+	{
+		this->add_subscription<geometry_msgs::msg::PoseStamped>("/robot_test/pose", this->get_input_state(), 0);
+		this->add_publisher<geometry_msgs::msg::TwistStamped>("/ds/desired_twist", this->get_output_state());
+
+		std::shared_ptr<DynamicalSystems::Linear<StateRepresentation::CartesianState> > move_dynamic = std::make_shared<DynamicalSystems::Linear<StateRepresentation::CartesianState> >(1);
+		move_dynamic->set_attractor(this->target_pose);
+		this->set_dynamic(move_dynamic);
+		return true;
+	}
+
+	bool is_terminated()
+	{
+		return this->get_input_state()->dist(*this->target_pose) < 1e-3;
+	}
+};
+
+class RandomAttractor : public Modulo::Core::Cell
+{
+private:
+	std::shared_ptr<StateRepresentation::CartesianPose> target_pose;
+	unsigned int counter;
+
+public:
+	template <typename DurationT>
+	explicit RandomAttractor(const std::string& node_name, const std::chrono::duration<int64_t, DurationT>& period):
+	Cell(node_name, period, false),
+	target_pose(std::make_shared<StateRepresentation::CartesianPose>("attractor", Eigen::Vector3d::Random(), Eigen::Quaterniond::UnitRandom())),
+	counter(0)
+	{}
+
+	bool on_configure()
+	{
+		this->add_publisher<geometry_msgs::msg::PoseStamped>("/ds/attractor", target_pose, 0);
+		this->add_asynchronous_transform_broadcaster(target_pose, 1ms);
+		return true;
+	}
+
+	void step()
+	{
+		this->target_pose->set_pose(Eigen::Vector3d::Random(), Eigen::Quaterniond::UnitRandom());
+		/*if (counter == 5)
+		{
+			counter = 0;
+			this->deactivate();
+		}
+		else
+		{
+			++counter;
+		}*/
+	}
+};
+
+template <typename StateMachineT>
+class EventHandler : public Modulo::Core::Cell
+{
+	private:
+		std::shared_ptr<StateMachineT> sm;
+
+		void topic_callback(const std_msgs::msg::Int64::SharedPtr msg) const
+	    {
+	      e =  msg->data;
+	    }
+	    rclcpp::Subscription<std_msgs::msg::Int64>::SharedPtr subscription_;
+
+	public:
+		explicit EventHandler(std::shared_ptr<StateMachineT> sent_sm):
+		Cell("event_handler", 1000ms, false)
+		{
+			this->sm = sent_sm;
+		}
+
+		bool on_configure()
+		{
+			this->subscription_ = this->create_subscription<std_msgs::msg::Int64>("/event_id", 10, std::bind(&EventHandler::topic_callback, this, std::placeholders::_1));
+			return true;
+		}
+
+		void step()
+		{
+			//this->sm->visit_current_states([](auto state) { std::cout << "Current state: " << state.c_str() << std::endl; });
+		    switch(e)
+		    {
+		    	case 3:
+		    	  this->sm->process_event(e_start{});
+		          break;
+		        case 0:
+		          this->sm->process_event(e0{});
+		          break;
+		        case 1:
+		          this->sm->process_event(e1{});
+		          break;
+		        case 2:
+		          this->sm->process_event(e2{});
+		          break;
+		    }
+		}
+};
+
+class ConsoleVisualizer : public Modulo::Visualizers::Visualizer
+{
+private:
+	std::shared_ptr<StateRepresentation::CartesianPose> robot_pose;
+	std::shared_ptr<StateRepresentation::CartesianTwist> desired_twist;
+	std::shared_ptr<StateRepresentation::Parameter<double> > ds_gain;
+
+public:
+	template <typename DurationT>
+	explicit ConsoleVisualizer(const std::string& node_name, const std::chrono::duration<int64_t, DurationT>& period) :
+	Visualizer(node_name, period, false),
+	robot_pose(std::make_shared<StateRepresentation::CartesianPose>("robot_test")),
+	desired_twist(std::make_shared<StateRepresentation::CartesianTwist>("robot_test"))
+	{}
+
+	bool on_configure()
+	{
+		this->add_subscription<geometry_msgs::msg::PoseStamped>("/robot_test/pose", this->robot_pose);
+		this->add_subscription<geometry_msgs::msg::TwistStamped>("/ds/desired_twist", this->desired_twist);
+		return true;
+	}
+
+	void step()
+	{
+		std::ostringstream os;
+		os << std::endl;
+		os << "##### DS TWIST #####" << std::endl;
+		os << *this->desired_twist << std::endl;
+
+		os << "##### ROBOT POSE #####" << std::endl;
+		os << *this->robot_pose << std::endl;
+
+		RCLCPP_INFO(get_logger(), "%s", os.str().c_str());
+	}
+};
+
+class SimulatedRobotInterface : public Modulo::Core::Cell
+{
+private:
+	std::shared_ptr<StateRepresentation::CartesianPose> robot_pose;
+	std::shared_ptr<StateRepresentation::CartesianTwist> desired_twist;
+	std::shared_ptr<StateRepresentation::CartesianPose> fixed_transform;
+	std::chrono::nanoseconds dt;
+
+public:
+	template <typename DurationT>
+	explicit SimulatedRobotInterface(const std::string& node_name, const std::chrono::duration<int64_t, DurationT>& period):
+	Cell(node_name, period, false),
+	robot_pose(std::make_shared<StateRepresentation::CartesianPose>("robot_test", Eigen::Vector3d::Random(), Eigen::Quaterniond::UnitRandom())),
+	desired_twist(std::make_shared<StateRepresentation::CartesianTwist>("robot_test")),
+	dt(period)
+	{}
+
+	bool on_configure()
+	{
+		this->add_subscription<geometry_msgs::msg::TwistStamped>("/ds/desired_twist", this->desired_twist, 0);
+		this->add_publisher<geometry_msgs::msg::PoseStamped>("/robot_test/pose", this->robot_pose, 0);
+		return true;
+	}
+
+	void step()
+	{
+		if(!this->desired_twist->is_empty())
+		{
+			*this->robot_pose = dt * *this->desired_twist + *this->robot_pose;
+		}
+		this->send_transform(this->robot_pose);
+	}
+};
+
+/**
+ * A lifecycle node has the same node API
+ * as a regular node. This means we can spawn a
+ * node, give it a name and add it to the executor.
+ */
+int main(int argc, char * argv[])
+{
+	// force flush of the stdout buffer.
+	// this ensures a correct sync of all prints
+	// even when executed simultaneously within the launch file.
+	setvbuf(stdout, NULL, _IONBF, BUFSIZ);
+
+	rclcpp::init(argc, argv);
+	rclcpp::executors::SingleThreadedExecutor exe;
+
+	std::shared_ptr<MoveAction> mh = std::make_shared<MoveAction>("move_home", 250us);
+	std::shared_ptr<MoveAction> ma1 = std::make_shared<MoveAction>("move_action1", 250us);
+	std::shared_ptr<MoveAction> ma2 = std::make_shared<MoveAction>("move_action2", 250us);
+	std::shared_ptr<SimulatedRobotInterface> sri = std::make_shared<SimulatedRobotInterface>("robot_interface", 250us);
+
+	exe.add_node(mh->get_node_base_interface());
+	exe.add_node(ma1->get_node_base_interface());
+	exe.add_node(ma2->get_node_base_interface());
+	exe.add_node(sri->get_node_base_interface());
+	
+	std::list<std::string> monitored_nodes = {"move_home", "move_action1", "move_action2", "robot_interface"};
+	std::shared_ptr<Modulo::Monitors::Monitor> mo = std::make_shared<Modulo::Monitors::Monitor>("monitor", monitored_nodes, 1s);
+	exe.add_node(mo->get_node_base_interface());
+	
+	static const auto start = [mh]
+	{ 
+		std::cout << "Starting" << std::endl;
+		mh->activate();
+	};
+
+	static const auto act_home = [ma1, ma2, mh]
+	{ 
+		std::cout << "act_home: Activate move_home" << std::endl; 
+		ma1->deactivate();
+		ma2->deactivate();
+		mh->activate();
+	};
+
+	static const auto act_action1 = [ma1, ma2, mh]
+	{ 
+		std::cout << "act_action1: Activate move_action1" << std::endl; 
+		ma2->deactivate();
+		mh->deactivate();
+		ma1->activate();
+	};
+
+	static const auto act_action2 = [ma1, ma2, mh]
+	{ 
+		std::cout << "act_action2: Activate move_action2" << std::endl;
+		mh->deactivate();
+		ma1->deactivate();
+		ma2->activate();
+	};
+
+	struct state_machine 
+	{
+		auto operator()() const noexcept {
+		  using namespace sml;
+		  return make_transition_table(
+
+		  	*"idle"_s + event<e_start> / start = "move_home"_s,
+		    "move_home"_s + event<e1> / act_action1 = "move_action1"_s,
+		    "move_home"_s + event<e2> / act_action2 = "move_action2"_s,
+		    "move_action1"_s + event<e0> / act_home = "move_home"_s,
+		    "move_action1"_s + event<e2> / act_action2 = "move_action2"_s,
+		    "move_action2"_s + event<e0> / act_home = "move_home"_s,
+		    "move_action2"_s + event<e1> / act_action1 = "move_action1"_s
+		  );
+		}
+	};
+
+	std::shared_ptr<sml::sm<state_machine>> sm = std::make_shared<sml::sm<state_machine>>();
+	std::shared_ptr<EventHandler<sml::sm<state_machine>>> eh = std::make_shared<EventHandler<sml::sm<state_machine>>>(sm);
+	exe.add_node(eh->get_node_base_interface());
+
+
+    exe.spin();
+    rclcpp::shutdown();
+	return 0;
+}
---
+++ source/packages/modulo_core/tests/testSetEvent.cpp
@@ -0,0 +1,46 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/int64.hpp"
+
+using namespace std::chrono_literals;
+
+class EventPublisher : public rclcpp::Node
+{
+  public:
+    EventPublisher()
+    : Node("event_publisher"), count_(0)
+    {
+      publisher_ = this->create_publisher<std_msgs::msg::Int64>("/event_id", 10);
+      timer_ = this->create_wall_timer(
+      500ms, std::bind(&EventPublisher::timer_callback, this));
+    }
+
+  private:
+    void timer_callback()
+    {
+      int event;
+      auto message = std_msgs::msg::Int64();
+      std::cout << "Trigger event (3: start, 0: home, 1: action1, 2: action2): "; std::cin >> event;
+
+      message.data = event;
+      publisher_->publish(message);
+    }
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::Int64>::SharedPtr publisher_;
+    size_t count_;
+  };
+
+  int main(int argc, char * argv[])
+  {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<EventPublisher>());
+    rclcpp::shutdown();
+    return 0;
+  }
+
+
+
