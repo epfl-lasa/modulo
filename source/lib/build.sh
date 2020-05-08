6f6eeaf8fe1e2cf793fcef40e942dab522e39e97
Baptiste Busch <baptiste@aica.tech>
2020-05-08 09:50:31 +0200

    Add -j argument to speed up building

--- source/lib/build.sh
+++ source/lib/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CURRPATH=${PWD}
 # cpp
-cd "${CURRPATH}/state_representation/cpp" && mkdir -p build && cd build && cmake .. && make && sudo make install
-cd "${CURRPATH}/dynamical_systems" && mkdir -p build && cd build && cmake .. && make && sudo make install
+cd "${CURRPATH}/state_representation/cpp" && mkdir -p build && cd build && cmake .. && make -j && sudo make install
+cd "${CURRPATH}/dynamical_systems" && mkdir -p build && cd build && cmake .. && make -j && sudo make install
 # python
 cd "${CURRPATH}/state_representation/python" && pip3 install -U -r requirements.txt && python3 setup.py develop --user
