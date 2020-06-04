ecd5a91484c81c07f89156b00372f59894e3a724
Baptiste Busch <baptiste.busch@gmail.com>
2020-06-04 12:06:09 +0200

    Changes the way the ds is calculated

--- source/lib/dynamical_systems/src/Circular.cpp
+++ source/lib/dynamical_systems/src/Circular.cpp
@@ -27,18 +27,16 @@ namespace DynamicalSystems
 		Eigen::Vector3d linear_velocity;
 		linear_velocity(2) = -this->get_gain() * pose.get_position()(2);
 
-		float radius = sqrt(pose.get_position()(0) * pose.get_position()(0) + pose.get_position()(1) * pose.get_position()(1));
-		float theta = atan2(pose.get_position()(1), pose.get_position()(0));
-		float angular_gain = this->get_circular_velocity();
-
 		std::vector<double> radiuses = this->get_radiuses();
-		double radius_desired = (radiuses[0] * radiuses[1]) / sqrt(radiuses[0]*radiuses[0]*sin(theta)*sin(theta) + radiuses[1]*radiuses[1]*cos(theta)*cos(theta));
 
-		double dradius = -this->get_gain() * (radius - radius_desired);
-		double dtheta = -angular_gain * exp(-dradius * dradius) / radius_desired;
+		double a2 = radiuses[0]*radiuses[0];
+		double b2 = radiuses[1]*radiuses[1];
+		double dradius = -this->get_gain() * radiuses[0] * radiuses[1] * (pose.get_position()[0]*pose.get_position()[0] / a2 + pose.get_position()[1]*pose.get_position()[1] / b2 - 1);
+		double tangeant_velocity_x = - radiuses[0] / radiuses[1] * pose.get_position()[1];
+		double tangeant_velocity_y = radiuses[1] / radiuses[0] * pose.get_position()[0];
 
-		linear_velocity(0) = dradius * cos(theta) - radius * dtheta * sin(theta);
-		linear_velocity(1) = dradius * sin(theta) + radius * dtheta * cos(theta);
+		linear_velocity(0) = this->get_circular_velocity() * tangeant_velocity_x + dradius * tangeant_velocity_y;
+		linear_velocity(1) = this->get_circular_velocity() * tangeant_velocity_y - dradius * tangeant_velocity_x;
 
 		velocity.set_linear_velocity(linear_velocity);
 		velocity.set_angular_velocity(Eigen::Vector3d::Zero());
