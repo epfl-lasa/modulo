a557a179a062292457b026ecd65d35347a59d571
Baptiste Busch <baptiste.busch@gmail.com>
2021-03-11 14:57:10 +0100

    fix: correct identation

--- source/packages/modulo_core/include/modulo_core/Exceptions/ParameterNotFoundException.hpp
+++ source/packages/modulo_core/include/modulo_core/Exceptions/ParameterNotFoundException.hpp
@@ -6,6 +6,7 @@
 namespace modulo::core::exceptions {
 class ParameterNotFoundException : public std::runtime_error {
 public:
-  explicit ParameterNotFoundException(const std::string& parameter_name) : runtime_error("Parameter " + parameter_name + " not found in the list of parameters"){};
+  explicit ParameterNotFoundException(const std::string& parameter_name) :
+  	runtime_error("Parameter " + parameter_name + " not found in the list of parameters"){};
 };
 }// namespace modulo::core::exceptions
