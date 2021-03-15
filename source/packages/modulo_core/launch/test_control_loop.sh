e38eedbf97195270a18786eeb13d28fd87670b8c
Baptiste Busch <baptiste.busch@gmail.com>
2021-03-15 11:03:19 +0100

    fix: launch the ft_sensor node

--- source/packages/modulo_core/launch/test_control_loop.sh
+++ source/packages/modulo_core/launch/test_control_loop.sh
@@ -2,8 +2,10 @@
 # ros2 param set /robot_interface damping_eigenvalues "[10.0, 5.0, 5.0, 2.0, 1.0, 1.0]"
 ros2 param set /robot_interface damping_eigenvalues "[50.0, 25.0, 25.0, 2.0, 1.0, 1.0]"
 
+ros2 lifecycle set /ft_sensor_interface configure
 ros2 lifecycle set /robot_interface configure
 ros2 lifecycle set /motion_generator configure
 
+ros2 lifecycle set /ft_sensor_interface activate
 ros2 lifecycle set /robot_interface activate
 ros2 lifecycle set /motion_generator activate
