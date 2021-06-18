996d930c83e5055b3c6478ab5a97a85603a3ed6c
Enrico Eberhard <32450951+eeberhard@users.noreply.github.com>
2021-06-18 14:44:58 +0200

    Revise Dockerfile and build / run scripts (#10)
    * Simply Dockerfile based on upstream
    aica-technology/ros2_ws image, meaning
    that the apt-key, entrypoint and cmd
    lines can be removed
    
    * Remove unused config directory
    
    * Build initial ros2_ws as the user
    rather than root, so that permissions
    are consistent
    
    * Use specific name for image in build script
    with aica-technology namespace rather than
    generating from the folder name
    
    * Use build flags to reduce code duplication
    in the --no-cache case
    
    * Use DOCKER_BUILDKIT
    
    * Minor formatting fixes in bash scripts

--- build.sh
+++ build.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 ROS_VERSION=foxy
 
-REBUILD=0
+IMAGE_NAME=aica-technology/modulo
+IMAGE_TAG=latest
+
+BUILD_FLAGS=()
 
 while getopts 'r' opt; do
-    case $opt in
-        r) REBUILD=1 ;;
-        *) echo 'Error in command line parsing' >&2
-           exit 1
-    esac
+  case $opt in
+    r) BUILD_FLAGS+=(--no-cache) ;;
+    *) echo 'Error in command line parsing' >&2
+       exit 1
+  esac
 done
 shift "$(( OPTIND - 1 ))"
 
-NAME=$(echo "${PWD##*/}" | tr _ -)
-TAG="latest"
+BUILD_FLAGS+=(--build-arg ROS_VERSION="${ROS_VERSION}")
+BUILD_FLAGS+=(-t "${IMAGE_NAME}":"${IMAGE_TAG}")
 
-if [ "$REBUILD" -eq 1 ]; then
-	docker build \
-    	--no-cache \
-    	--build-arg ROS_VERSION="${ROS_VERSION}" \
- 		-t "${NAME}:${TAG}" .
-else
-	docker build \
-	    --build-arg ROS_VERSION="${ROS_VERSION}" \
-	    -t "${NAME}:${TAG}" .
-fi
+DOCKER_BUILDKIT=1 docker build "${BUILD_FLAGS[@]}" .
