384beb4b36608da9fd2b79bffdedfbf99bb16910
Baptiste Busch <baptiste.busch@gmail.com>
2021-09-22 10:04:39 +0200

    fix: add the help message to show usage

--- build.sh
+++ build.sh
@@ -4,14 +4,25 @@ ROS_VERSION=foxy
 BUILD_PROD=false
 IMAGE_NAME=aica-technology/modulo
 
-PARAM_BUILD_FLAGS=()
+HELP_MESSAGE="Usage: build.sh [-p] [-r]
 
+Options:
+  -p, --production       Build the production ready image on top of
+                         of the development one.
+
+  -r, --rebuild          Rebuild the image(s) using the docker
+                         --no-cache option
+"
+
+PARAM_BUILD_FLAGS=()
 while [[ $# -gt 0 ]]; do
   opt="$1"
   case $opt in
     -p|--production) BUILD_PROD=true ; shift ;;
     -r|--rebuild) PARAM_BUILD_FLAGS+=(--no-cache) ; shift ;;
+    -h|--help) echo "${HELP_MESSAGE}" ; exit 0 ;;
     *) echo 'Error in command line parsing' >&2
+       echo -e "\n${HELP_MESSAGE}"
        exit 1
   esac
 done
