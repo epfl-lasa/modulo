87f6b459b1e4131de2e88c67d3f602c013319509
Baptiste Busch <baptiste.busch@gmail.com>
2021-09-22 10:25:50 +0200

    Improve the script to build also the production image (#27)

--- build.sh
+++ build.sh
@@ -1,23 +1,44 @@
 #!/bin/bash
 ROS_VERSION=foxy
 
-TARGET=development
+BUILD_PROD=false
 IMAGE_NAME=aica-technology/modulo
 
-BUILD_FLAGS=()
+HELP_MESSAGE="Usage: build.sh [-p] [-r]
 
-while getopts 'r' opt; do
+Options:
+  -p, --production       Build the production ready image on top of
+                         of the development one.
+
+  -r, --rebuild          Rebuild the image(s) using the docker
+                         --no-cache option
+"
+
+PARAM_BUILD_FLAGS=()
+while [[ $# -gt 0 ]]; do
+  opt="$1"
   case $opt in
-    r) BUILD_FLAGS+=(--no-cache) ;;
+    -p|--production) BUILD_PROD=true ; shift ;;
+    -r|--rebuild) PARAM_BUILD_FLAGS+=(--no-cache) ; shift ;;
+    -h|--help) echo "${HELP_MESSAGE}" ; exit 0 ;;
     *) echo 'Error in command line parsing' >&2
+       echo -e "\n${HELP_MESSAGE}"
        exit 1
   esac
 done
-shift "$(( OPTIND - 1 ))"
 
-
+BUILD_FLAGS=()
 BUILD_FLAGS+=(--build-arg ROS_VERSION="${ROS_VERSION}")
-BUILD_FLAGS+=(-t "${IMAGE_NAME}/${TARGET}":"${ROS_VERSION}")
+BUILD_FLAGS+=(-t "${IMAGE_NAME}/development":"${ROS_VERSION}")
+BUILD_FLAGS+=("${PARAM_BUILD_FLAGS[@]}")
 
-docker pull ghcr.io/aica-technology/ros2-ws:"${ROS_VERSION}"
-DOCKER_BUILDKIT=1 docker build --file ./Dockerfile.${TARGET} "${BUILD_FLAGS[@]}" .
+docker pull ghcr.io/aica-technology/ros2-control-libraries:"${ROS_VERSION}"
+DOCKER_BUILDKIT=1 docker build --file ./Dockerfile.development "${BUILD_FLAGS[@]}" .
+
+if [ $BUILD_PROD = true ]; then
+  BUILD_FLAGS=()
+  BUILD_FLAGS+=(--build-arg ROS_VERSION="${ROS_VERSION}")
+  BUILD_FLAGS+=(-t "${IMAGE_NAME}":"${ROS_VERSION}")
+  BUILD_FLAGS+=("${PARAM_BUILD_FLAGS[@]}")
+  DOCKER_BUILDKIT=1 docker build --file ./Dockerfile.production "${BUILD_FLAGS[@]}" .
+fi
