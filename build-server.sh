bf9715e5622759e64e9cebeced6da48fda1e39a5
Dominic Reber <dominic.reber@epfl.ch>
2021-10-01 22:17:55 +0200

    Update docker paradigm

--- build-server.sh
+++ build-server.sh
@@ -2,16 +2,20 @@
 ROS_VERSION=foxy
 
 BUILD_PROD=false
+SERVE_REMOTE=false
+
 IMAGE_NAME=epfl-lasa/modulo
+REMOTE_SSH_PORT=4440
 
 HELP_MESSAGE="Usage: build.sh [-p] [-r]
-
 Options:
   -p, --production       Build the production ready image on top of
                          of the development one.
-
   -r, --rebuild          Rebuild the image(s) using the docker
                          --no-cache option
+  -v, --verbose          Use the verbose option during the building
+                         process
+  -s, --serve            Start the remove development server
 "
 
 PARAM_BUILD_FLAGS=()
@@ -20,6 +24,8 @@ while [[ $# -gt 0 ]]; do
   case $opt in
     -p|--production) BUILD_PROD=true ; shift ;;
     -r|--rebuild) PARAM_BUILD_FLAGS+=(--no-cache) ; shift ;;
+    -v|--verbose) PARAM_BUILD_FLAGS+=(--progress=plain) ; shift ;;
+    -s|--serve) SERVE_REMOTE=true ; shift ;;
     -h|--help) echo "${HELP_MESSAGE}" ; exit 0 ;;
     *) echo 'Error in command line parsing' >&2
        echo -e "\n${HELP_MESSAGE}"
@@ -29,16 +35,15 @@ done
 
 BUILD_FLAGS=()
 BUILD_FLAGS+=(--build-arg ROS_VERSION="${ROS_VERSION}")
-BUILD_FLAGS+=(-t "${IMAGE_NAME}/development":"${ROS_VERSION}")
 BUILD_FLAGS+=("${PARAM_BUILD_FLAGS[@]}")
 
 docker pull ghcr.io/aica-technology/ros2-control-libraries:"${ROS_VERSION}"
-DOCKER_BUILDKIT=1 docker build --file ./Dockerfile.development "${BUILD_FLAGS[@]}" .
+DOCKER_BUILDKIT=1 docker build --target development -t "${IMAGE_NAME}/development:latest" "${BUILD_FLAGS[@]}" .
 
-if [ $BUILD_PROD = true ]; then
-  BUILD_FLAGS=()
-  BUILD_FLAGS+=(--build-arg ROS_VERSION="${ROS_VERSION}")
-  BUILD_FLAGS+=(-t "${IMAGE_NAME}":"${ROS_VERSION}")
-  BUILD_FLAGS+=("${PARAM_BUILD_FLAGS[@]}")
-  DOCKER_BUILDKIT=1 docker build --file ./Dockerfile.production "${BUILD_FLAGS[@]}" .
+if [ "${BUILD_PROD}" = true ]; then
+  DOCKER_BUILDKIT=1 docker build --target production -t "${IMAGE_NAME}:latest" "${BUILD_FLAGS[@]}" .
+fi
+
+if [ "${SERVE_REMOTE}" = true ]; then
+  aica-docker server "${IMAGE_NAME}/development:latest" -u ros2 -p "${REMOTE_SSH_PORT}"
 fi
