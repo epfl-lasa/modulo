533cbc6e91d6c9b6c49543ca3ad737e3bb61be44
Baptiste Busch <baptiste.busch@gmail.com>
2021-10-10 14:24:20 +0200

    fix: change the underlying message type to UInt8MultyArray

--- source/modulo_core/include/modulo_core/communication/EncodedState.hpp
+++ source/modulo_core/include/modulo_core/communication/EncodedState.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <std_msgs/msg/byte_multi_array.hpp>
+#include <std_msgs/msg/u_int8_multi_array.hpp>
 
 namespace modulo::core {
-	typedef std_msgs::msg::ByteMultiArray EncodedState;
+	typedef std_msgs::msg::UInt8MultiArray EncodedState;
 }
