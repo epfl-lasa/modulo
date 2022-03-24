76fd43d944fa53adfd408fe713fa67e042d18ec3
Dominic Reber <dominic.reber@epfl.ch>
2022-03-24 09:14:38 +0100

    Remove code

--- source/modulo_components/include/modulo_components/utilities/predicate_type.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <variant>
-
-namespace modulo_components::utilities {
-
-typedef std::variant<bool, std::function<bool(void)>> PredicateType;
-
-}// namespace modulo_components::utilities
--- source/modulo_components/src/ComponentInterface.cpp
+++ source/modulo_components/src/ComponentInterface.cpp
@@ -1,9 +1,7 @@
 #include "modulo_components/ComponentInterface.h"
 
 
-
 namespace modulo_components {
 
 
-
-} //namespace modulo_components
+}// namespace modulo_components
