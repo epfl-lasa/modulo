6da4dcd28091dcc87fd6fd7d49ebc6fce316616d
domire8 <71256590+domire8@users.noreply.github.com>
2022-04-01 17:01:00 +0200

    Add MessagePairInterface and MessagePair including tests (#43)
    * Add MessagePairInterface and MessagePair including tests
    
    * Minor improvements
    
    * Rename functions

---
+++ source/modulo_new_core/src/communication/MessagePairInterface.cpp
@@ -0,0 +1,11 @@
+#include "modulo_new_core/communication/MessagePairInterface.hpp"
+
+namespace modulo_new_core::communication {
+
+MessagePairInterface::MessagePairInterface(MessageType type) : type_(type) {}
+
+MessageType MessagePairInterface::get_type() const {
+  return this->type_;
+}
+
+}// namespace modulo_new_core::communication
