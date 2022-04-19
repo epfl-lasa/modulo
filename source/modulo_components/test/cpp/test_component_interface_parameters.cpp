eb01e4752d0029d274b8b20464cc643971b9b630
Enrico Eberhard <enrico.eberhard@gmail.com>
2022-04-19 20:21:32 +0200

    Extend tests

--- source/modulo_components/test/cpp/test_component_interface_parameters.cpp
+++ source/modulo_components/test/cpp/test_component_interface_parameters.cpp
@@ -17,6 +17,7 @@ public:
   using ComponentInterface<rclcpp::Node>::add_parameter;
   using ComponentInterface<rclcpp::Node>::get_parameter;
   using ComponentInterface<rclcpp::Node>::get_parameter_value;
+  using ComponentInterface<rclcpp::Node>::set_parameter_value;
   using ComponentInterface<rclcpp::Node>::parameter_map_;
 
   bool validate_parameter(const std::shared_ptr<state_representation::ParameterInterface>&) override {
@@ -30,6 +31,10 @@ public:
 
   rcl_interfaces::msg::SetParametersResult set_ros_parameter(const rclcpp::Parameter& parameter) {
     return rclcpp::Node::set_parameter(parameter);
+  }
+
+  std::string get_parameter_description(const std::string& name) {
+    return rclcpp::Node::describe_parameter(name).description;
   }
 
   bool validate_was_called = false;
@@ -48,6 +53,7 @@ protected:
 
   void SetUp() override {
     component_ = std::make_shared<ComponentInterfacePublicInterface>(rclcpp::NodeOptions());
+    param_ = state_representation::make_shared_parameter("test", 1);
   }
 
   template<typename T>
@@ -58,27 +64,92 @@ protected:
   }
 
   std::shared_ptr<ComponentInterfacePublicInterface> component_;
+  std::shared_ptr<state_representation::Parameter<int>> param_;
 };
 
-
 TEST_F(ComponentInterfaceParameterTest, AddParameter) {
-  EXPECT_THROW(component_->get_parameter("test"), state_representation::exceptions::InvalidParameterException);
+  EXPECT_THROW(auto discard = component_->get_parameter("test"),
+               state_representation::exceptions::InvalidParameterException);
   EXPECT_THROW(component_->get_ros_parameter("test"), rclcpp::exceptions::ParameterNotDeclaredException);
-  auto param = state_representation::make_shared_parameter("test", 1);
-  component_->add_parameter(param);
+  component_->add_parameter(param_, "Test parameter");
 
   // Adding the parameter should declare and set the value and call the validation function
   EXPECT_TRUE(component_->validate_was_called);
-  EXPECT_NO_THROW(component_->get_parameter("test"));
+  EXPECT_NO_THROW(auto discard = component_->get_parameter("test"));
   EXPECT_NO_THROW(component_->get_ros_parameter("test"));
   expect_parameter_value<int>(1);
+}
+
+TEST_F(ComponentInterfaceParameterTest, AddNameValueParameter) {
+  EXPECT_THROW(auto discard = component_->get_parameter("test"),
+               state_representation::exceptions::InvalidParameterException);
+  EXPECT_THROW(component_->get_ros_parameter("test"), rclcpp::exceptions::ParameterNotDeclaredException);
+  component_->add_parameter("test", 1, "Test parameter");
+
+  // Adding the parameter should declare and set the value and call the validation function
+  EXPECT_TRUE(component_->validate_was_called);
+  EXPECT_NO_THROW(auto discard = component_->get_parameter("test"));
+  EXPECT_NO_THROW(component_->get_ros_parameter("test"));
+  expect_parameter_value<int>(1);
+}
+
+TEST_F(ComponentInterfaceParameterTest, AddParameterAgain) {
+  EXPECT_THROW(auto
+                   discard = component_->get_parameter("test"),
+               state_representation::exceptions::InvalidParameterException);
+  EXPECT_THROW(component_->get_ros_parameter("test"), rclcpp::exceptions::ParameterNotDeclaredException);
+  component_->add_parameter(param_, "Test parameter");
+
+  // Adding an existing parameter again should just set the value
+  component_->validate_was_called = false;
+  EXPECT_NO_THROW(component_->add_parameter("test", 2, "foo"));
+  EXPECT_TRUE(component_->validate_was_called);
+  expect_parameter_value<int>(2);
+  EXPECT_EQ(param_->get_value(), 2);
+}
+
+TEST_F(ComponentInterfaceParameterTest, SetParameter) {
+  // setting before adding should not work
+  EXPECT_THROW(component_->set_parameter_value("test", 1), rclcpp::exceptions::ParameterNotDeclaredException);
+
+  // validation should not be invoked as the parameter did not exist
+  EXPECT_FALSE(component_->validate_was_called);
+  EXPECT_THROW(component_->get_ros_parameter("test"), rclcpp::exceptions::ParameterNotDeclaredException);
+
+  component_->add_parameter(param_, "Test parameter");
+
+  // Setting the parameter value should call the validation function and update the referenced value
+  component_->validate_was_called = false;
+  EXPECT_NO_THROW(component_->set_parameter_value("test", 2));
+  EXPECT_TRUE(component_->validate_was_called);
+  expect_parameter_value<int>(2);
+  EXPECT_EQ(param_->get_value(), 2);
+
+  // If the validation function returns false, setting the parameter value should _not_ update the referenced value
+  component_->validate_was_called = false;
+  component_->validation_return_value = false;
+  EXPECT_THROW(component_->set_parameter_value("test", 3), state_representation::exceptions::InvalidParameterException);
+  EXPECT_TRUE(component_->validate_was_called);
+  expect_parameter_value<int>(2);
+  EXPECT_EQ(param_->get_value(), 2);
+
+  // Setting a value with an incompatible type should not update the parameter
+  EXPECT_THROW(component_->set_parameter_value<std::string>("test", "foo"),
+               state_representation::exceptions::InvalidParameterException);
+  EXPECT_TRUE(component_->validate_was_called);
+  expect_parameter_value<int>(2);
+  EXPECT_EQ(param_->get_value(), 2);
+}
+
+TEST_F(ComponentInterfaceParameterTest, SetParameterROS) {
+  component_->add_parameter(param_, "Test parameter");
 
   // Setting the parameter value should call the validation function and update the referenced value
   component_->validate_was_called = false;
   component_->set_ros_parameter({"test", 2});
   EXPECT_TRUE(component_->validate_was_called);
   expect_parameter_value<int>(2);
-  EXPECT_EQ(param->get_value(), 2);
+  EXPECT_EQ(param_->get_value(), 2);
 
   // If the validation function returns false, setting the parameter value should _not_ update the referenced value
   component_->validate_was_called = false;
@@ -86,7 +157,14 @@ TEST_F(ComponentInterfaceParameterTest, AddParameter) {
   component_->set_ros_parameter({"test", 3});
   EXPECT_TRUE(component_->validate_was_called);
   expect_parameter_value<int>(2);
-  EXPECT_EQ(param->get_value(), 2);
+  EXPECT_EQ(param_->get_value(), 2);
+}
+
+TEST_F(ComponentInterfaceParameterTest, GetParameterDescription) {
+  component_->add_parameter(param_, "Test parameter");
+  EXPECT_STREQ(component_->get_parameter_description("test").c_str(), "Test parameter");
+
+  EXPECT_THROW(component_->get_parameter_description("foo"), rclcpp::exceptions::ParameterNotDeclaredException);
 }
 
 } // namespace modulo_components
