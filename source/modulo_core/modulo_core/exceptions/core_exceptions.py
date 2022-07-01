bb68ba5a9d9b673dfe5f7ccf6ec8818da2e0931a
Dominic Reber <dominic.reber@epfl.ch>
2022-07-01 11:28:05 +0200

    Create modulo core errors in Python

---
+++ source/modulo_core/modulo_core/exceptions/core_exceptions.py
@@ -0,0 +1,27 @@
+class CoreError(Exception):
+    """
+    A base class for all core exceptions.
+    """
+
+    def __init__(self, message: str):
+        super().__init__(message)
+
+
+class MessageTranslationError(CoreError):
+    """
+    An exception class to notify that the translation of a ROS message failed.
+    """
+
+    def __init__(self, message: str):
+        super().__init__(message)
+
+
+class ParameterTranslationError(CoreError):
+    """
+    An exception class to notify incompatibility when translating parameters from different sources. This is an
+    exception class to be thrown if there is a problem while translating from a ROS parameter to a state_representation
+    parameter and vice versa.
+    """
+
+    def __init__(self, message: str):
+        super().__init__(message)
