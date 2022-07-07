2c95d92f8c2a7d21b081c085bac2f189dd2a133e
Dominic Reber <dominic.reber@epfl.ch>
2022-07-07 12:38:10 +0200

    Try catch publishing in PublisherInterface

--- source/modulo_core/src/communication/PublisherInterface.cpp
+++ source/modulo_core/src/communication/PublisherInterface.cpp
@@ -81,28 +81,32 @@ void PublisherInterface::deactivate() {
 }
 
 void PublisherInterface::publish() {
-  if (this->message_pair_ == nullptr) {
-    throw exceptions::NullPointerException("Message pair is not set, nothing to publish");
-  }
-  switch (this->message_pair_->get_type()) {
-    case MessageType::BOOL:
-      this->publish(this->message_pair_->write<std_msgs::msg::Bool, bool>());
-      break;
-    case MessageType::FLOAT64:
-      this->publish(this->message_pair_->write<std_msgs::msg::Float64, double>());
-      break;
-    case MessageType::FLOAT64_MULTI_ARRAY:
-      this->publish(this->message_pair_->write<std_msgs::msg::Float64MultiArray, std::vector<double>>());
-      break;
-    case MessageType::INT32:
-      this->publish(this->message_pair_->write<std_msgs::msg::Int32, int>());
-      break;
-    case MessageType::STRING:
-      this->publish(this->message_pair_->write<std_msgs::msg::String, std::string>());
-      break;
-    case MessageType::ENCODED_STATE:
-      this->publish(this->message_pair_->write<EncodedState, state_representation::State>());
-      break;
+  try {
+    if (this->message_pair_ == nullptr) {
+      throw exceptions::NullPointerException("Message pair is not set, nothing to publish");
+    }
+    switch (this->message_pair_->get_type()) {
+      case MessageType::BOOL:
+        this->publish(this->message_pair_->write<std_msgs::msg::Bool, bool>());
+        break;
+      case MessageType::FLOAT64:
+        this->publish(this->message_pair_->write<std_msgs::msg::Float64, double>());
+        break;
+      case MessageType::FLOAT64_MULTI_ARRAY:
+        this->publish(this->message_pair_->write<std_msgs::msg::Float64MultiArray, std::vector<double>>());
+        break;
+      case MessageType::INT32:
+        this->publish(this->message_pair_->write<std_msgs::msg::Int32, int>());
+        break;
+      case MessageType::STRING:
+        this->publish(this->message_pair_->write<std_msgs::msg::String, std::string>());
+        break;
+      case MessageType::ENCODED_STATE:
+        this->publish(this->message_pair_->write<EncodedState, state_representation::State>());
+        break;
+    }
+  } catch (const exceptions::CoreException& ex) {
+    throw exceptions::CoreException(ex);
   }
 }
 
