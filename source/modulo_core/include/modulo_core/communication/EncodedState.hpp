8587b6f311c8502b9649f38af9df945c5cda5619
Enrico Eberhard <enrico.eberhard@gmail.com>
2022-07-21 15:24:42 +0200

    Remove all sources

--- source/modulo_core/include/modulo_core/communication/EncodedState.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <std_msgs/msg/u_int8_multi_array.hpp>
-
-namespace modulo::core {
-	typedef std_msgs::msg::UInt8MultiArray EncodedState;
-}
--- source/modulo_core/src/communication/CommunicationHandler.cpp
+++
@@ -1,5 +0,0 @@
-#include "modulo_core/communication/CommunicationHandler.hpp"
-
-namespace modulo::core::communication {
-CommunicationHandler::CommunicationHandler(const CommunicationType& type) : type_(type) {}
-}// namespace modulo::core::communication
