394680b35bde778d986bf656a88589365c6aae8c
domire8 <domire8@users.noreply.github.com>
2022-09-15 06:27:23 +0000

    deploy: def34d3a19f642c86dd4bcce3ac7f6fb008159d6

--- versions/develop/search/functions_2.js
+++ versions/develop/search/functions_2.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['deactivate_0',['deactivate',['../classmodulo__core_1_1communication_1_1_publisher_interface.html#a2570447d8280ab4f1bc47e8c020082f3',1,'modulo_core::communication::PublisherInterface']]]
+  ['deactivate_0',['deactivate',['../classmodulo__core_1_1communication_1_1_publisher_interface.html#a2570447d8280ab4f1bc47e8c020082f3',1,'modulo_core::communication::PublisherInterface']]],
+  ['declare_5finput_1',['declare_input',['../classmodulo__components_1_1_component_interface.html#a5764c4fb141d551d44d31858b9e0d84e',1,'modulo_components::ComponentInterface']]],
+  ['declare_5foutput_2',['declare_output',['../classmodulo__components_1_1_component_interface.html#ad2a113d2874d1ee0ca3b43f2892a22fc',1,'modulo_components::ComponentInterface']]]
 ];
