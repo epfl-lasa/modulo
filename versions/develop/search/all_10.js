7cf641938decc9f0f94695fc77dcf49e58a21af0
domire8 <domire8@users.noreply.github.com>
2022-09-22 06:00:32 +0000

    deploy: 965a960273cc52253d451b3a423d3d17863b2648

--- versions/develop/search/all_10.js
+++ versions/develop/search/all_10.js
@@ -4,6 +4,8 @@ var searchData=
   ['_7ecomponentinterface_1',['~ComponentInterface',['../classmodulo__components_1_1_component_interface.html#adf2b4c82aaa0fdc02f0a719c59a79729',1,'modulo_components::ComponentInterface']]],
   ['_7elifecyclecomponent_2',['~LifecycleComponent',['../classmodulo__components_1_1_lifecycle_component.html#a534d2620117f9b2420418d4d6ca48a12',1,'modulo_components::LifecycleComponent']]],
   ['_7emessagepairinterface_3',['~MessagePairInterface',['../classmodulo__core_1_1communication_1_1_message_pair_interface.html#a1b0177982d2972d701692b3c89705aae',1,'modulo_core::communication::MessagePairInterface']]],
-  ['_7epublisherinterface_4',['~PublisherInterface',['../classmodulo__core_1_1communication_1_1_publisher_interface.html#a1e670470b065bb1e7b1e5ef88eec5268',1,'modulo_core::communication::PublisherInterface']]],
-  ['_7esubscriptioninterface_5',['~SubscriptionInterface',['../classmodulo__core_1_1communication_1_1_subscription_interface.html#afd0d227d6533cafc7731f1fd52cb8d50',1,'modulo_core::communication::SubscriptionInterface']]]
+  ['_7epublisherhandler_4',['~PublisherHandler',['../classmodulo__core_1_1communication_1_1_publisher_handler.html#a9c2eafb61ee4d8e60a15e0af519be4d3',1,'modulo_core::communication::PublisherHandler']]],
+  ['_7epublisherinterface_5',['~PublisherInterface',['../classmodulo__core_1_1communication_1_1_publisher_interface.html#a1e670470b065bb1e7b1e5ef88eec5268',1,'modulo_core::communication::PublisherInterface']]],
+  ['_7esubscriptionhandler_6',['~SubscriptionHandler',['../classmodulo__core_1_1communication_1_1_subscription_handler.html#a3de45dc5da0ca5ae1de4a819851f4979',1,'modulo_core::communication::SubscriptionHandler']]],
+  ['_7esubscriptioninterface_7',['~SubscriptionInterface',['../classmodulo__core_1_1communication_1_1_subscription_interface.html#afd0d227d6533cafc7731f1fd52cb8d50',1,'modulo_core::communication::SubscriptionInterface']]]
 ];
